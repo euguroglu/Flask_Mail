93b6e314d413f01ed0d5434f1574b57dbc651004
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 16:00:52 +0300

    more recipient feature added

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'xxxx.smtp.gmal.com'
+app.config['MAIL_SERVER'] = 'smpt.itu.edu.tr'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'xxx@xmail.com'
-app.config['MAIL_PASSWORD'] = 'xxxxxxxxx'
+app.config['MAIL_USERNAME'] = '@itu.edu.tr'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEFAULT_SENDER'] = 'xxx@xmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = '@itu.edu.tr'
 
 mail = Mail()
 mail.init_app(app)
 
 @app.route('/')
 def index():
-    msg = Message('Hello From Flask',recipients=['enes@gmail.com'])
+    msg = Message('Hello From Flask',recipients=['@gmail.com'])
+    msg.add_recipient('@yahoo.com')
+    msg.body = "This is text of the mail"
+    msg.html = '<b>This is a html message</b>'
     mail.send(msg)
 
     return '<h1>Sent!</h1>'
