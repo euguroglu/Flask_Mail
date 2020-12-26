0c7880188c5e2b7397ab4aef8e6e5a6dbc7cef5f
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 16:26:17 +0300

    how to send attachment?

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'smpt.itu.edu.tr'
+app.config['MAIL_SERVER'] = 'smtp.itu.edu.tr'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = '@itu.edu.tr'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = '7'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEFAULT_SENDER'] = '@itu.edu.tr'
+app.config['MAIL_DEFAULT_SENDER'] = ('Enes U' ,'u@itu.edu.tr')
 
 mail = Mail()
 mail.init_app(app)
 
 @app.route('/')
 def index():
-    msg = Message('Hello From Flask',recipients=['@gmail.com'])
+    msg = Message('Hello From Flask',recipients=['u@gmail.com'],sender=('Enes','@itu.edu.tr'))
     msg.add_recipient('@yahoo.com')
     msg.body = "This is text of the mail"
-    msg.html = '<b>This is a html message</b>'
+    msg.html = '<b>Please see attached</b>'
+    #How to send attachement with e mail
+    with app.open_resource('convolutional_strategy.pdf') as pdf:
+        msg.attach('flask.pdf','application/pdf',pdf.read())
     mail.send(msg)
 
     return '<h1>Sent!</h1>'
